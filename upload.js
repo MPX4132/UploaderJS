e8554c0d01e561fedf217d24de2d35a6ae92ced0
Matias Barcenas <MPX.4132@Gmail.com>
2016-10-15 15:53:10 -0500

    Optimization, Attributes & Proprocessor
    Optimized the entire plugin to prevent recompilation of precomputed
    items.
    Switched the plugin to select with attributes, rather than classes.
    Added a proprocessor function, called after each attempt to upload a
    file.

--- upload.js
+++ upload.js
@@ -1,96 +1,91 @@
 /*
-uploader([ processor ]) 
-	- processor: A function called before sending the file.
-		> processor([formData [, $nameTextBox [, $row ]]]) : Returns whether or not to reject a file.
+uploader([ preprocessor ]) 
+	- preprocessor: A function called before sending the file.
+		> preprocessor([formData [, $nameTextBox [, $row ]]]) : Returns whether or not to reject a file.
 			- formData: The data to be sent, includes the key value pair to be sent
 				-> Key - File name (from textbox or file's original if none specified)
 				-> Value - File itself (includes original file name)
 			- $nameTextBox: A jQuery object holding the input textbox element for the file.
 			- $row: A jQuery object holding the entire row element for the file.
 */
-jQuery.fn.uploader = function(processor) {
-	$(this).each(function() {
-		$(this).find("table").bind("update", function(event) {
-			var $upldBody = $(this).find("tbody");
-			var $upldSubmit = $upldBody.find("button.upld-submit").attr("disabled", true);
-			
-			$upldBody.children().each(function(i, row) {
-				if ($(row).data("file")) {
-					$upldSubmit.attr("disabled", false);
-					return false;
-				}
-			});
-			
-			$(this)[($(this).find("tbody>tr").length - 1)? "fadeIn" : "fadeOut"]();
+jQuery.fn.uploader = function(preprocessor, proprocessor) {
+	var $upldTableRoot = $(this).find("table");
+	var $upldTableBody = $upldTableRoot.find("tbody");
+	var $upldTableSubmit = $upldTableBody.find("button[data-upld-submit]");
+	
+	$upldTableRoot.bind("update", function(event) {
+		// Assume everything was sent, so we'll disable the upload button.
+		$upldTableSubmit.attr("disabled", true);
+		
+		// Look for at least one that wasn't sent, in which case, we enable the upload button.
+		$upldTableBody.children().each(function(i, row) {
+			if ($(row).data("file")) { 
+				$upldTableSubmit.attr("disabled", false);
+				return false;
+			}
 		});
-
-		$(this).find("button.upld-submit").click(function(event) {
-			$(this).closest("tbody").children().each(function(i, row) {
-				if (!$(row).data("file")) return;
-				
-				var $upldTable = $(row).closest("table");
-				var $upldInputGroup = $(row).find("div.input-group"); 
-				var $upldIndicator = $upldInputGroup.find("span.upld-indicator");
-				var $upldDismiss = $upldInputGroup.find("button.upld-dismiss");
-				var $upldDismissIndicator = $upldInputGroup.find("button.upld-dismiss > span.glyphicon");
-				var $upldFilename = $upldInputGroup.find("input.upld-filename");
-				
-				var file = $(row).data("file");
-				var formData = new FormData();
-				
-				formData.append("name", $upldFilename.val() || file.name);
-				formData.append("file", file);
-
-				if (!processor || processor(formData, $upldFilename, $(row))) {
-					$.post({
-						url: "upload.php",
-						data: formData,
-						// cache: false, // Cache response? For JSON default is false
-						processData: false,
-		                contentType: false,
-						success: function(data) {
-
-		 					$upldIndicator.addClass();
-		 					$upldDismiss.addClass();
-		 					$upldDismissIndicator.addClass();
-		 					
-		 					$(row).data("file", null); // Clear the file
-		 					console.log(data);
-						},
-						error: function(event) {
-		 					console.log(event);
-						},
-						complete: function(xhr, result) {
-							var success = result == "success";
-							
-							$upldInputGroup.removeClass("has-warning has-error");
-							$upldIndicator.removeClass("glyphicon-chevron-up glyphicon-chevron-right glyphicon-repeat");
-							$upldDismiss.removeClass("btn-danger btn-warning");
-							$upldDismissIndicator.removeClass("glyphicon-trash");
-							
-							$upldFilename.attr("disabled", success);
-		 					$upldInputGroup.addClass(success? "has-success" : "has-error");
-		 					$upldIndicator.addClass(success? "glyphicon-ok" : "glyphicon-repeat");
-		 					$upldDismiss.addClass(success? "btn-success" : "btn-danger");
-		 					$upldDismissIndicator.addClass(success? "glyphicon-minus" : "glyphicon-trash");
-							
-							$upldTable.trigger("update");
-						}
-					});
-				} else {
-					$upldInputGroup.removeClass("has-error");
-					$upldIndicator.removeClass("glyphicon-chevron-up glyphicon-repeat");
-					$upldDismiss.removeClass("btn-danger");
-					
-					$upldIndicator.addClass("glyphicon-chevron-right");
-					$upldInputGroup.addClass("has-warning");
- 					$upldDismiss.addClass("btn-warning");
-				}
-			});
-			return false; // Stop propagation
-		});
-		$(this).find("table").hide();
+		
+		$upldTableRoot[($upldTableBody.find("tr").length - 1)? "fadeIn" : "fadeOut"]();
 	});
+
+	$upldTableSubmit.click(function(event) {
+		$upldTableBody.children().each(function(i, row) {
+			if (!$(row).data("file")) return; // If no file, skip.
+			
+			//var $upldTable = $(row).closest("table");
+			var $upldTableRowIndicator = $(row).find("span[data-upld-indicator]");
+			var $upldTableRowDismiss = $(row).find("button[data-upld-dismiss]");
+			var $upldTableRowDismissIndicator = $upldTableRowDismiss.find("span.glyphicon");
+			var $upldTableRowFilename = $(row).find("input[data-upld-filename]");
+			
+			var $upldTableRowGroup = $upldTableRowFilename.closest("div.input-group");
+			
+			var file = $(row).data("file");
+			var formData = new FormData();
+			
+			formData.append("name", $upldTableRowFilename.val() || file.name);
+			formData.append("file", file);
+
+			if (!preprocessor || preprocessor(formData, $upldTableRowFilename, $upldTableRowGroup, $(row))) {
+				$.post({
+					url: "upload.php",
+					data: formData,
+					// cache: false, // Cache response? For JSON default is false
+					processData: false,
+	                contentType: false,
+					success: function(data) {
+	 					$(row).data("file", null); // Clear the file
+	 					console.log(data);
+					},
+					error: function(event) {
+	 					console.log(event);
+					},
+					complete: function(xhr, result) {
+						var success = result == "success";
+						$upldTableRowFilename.attr("disabled", success);
+						
+						$upldTableRowIndicator.removeClass("glyphicon-chevron-up glyphicon-repeat");
+						$upldTableRowDismissIndicator.removeClass("glyphicon-trash");
+						$upldTableRowDismiss.removeClass("btn-warning");
+						
+	 					$upldTableRowIndicator.addClass(success? "glyphicon-ok" : "glyphicon-repeat");
+	 					$upldTableRowDismissIndicator.addClass(success? "glyphicon-minus" : "glyphicon-trash");
+	 					$upldTableRowDismiss.addClass(success? "btn-info" : "btn-warning");
+						
+						if (proprocessor) proprocessor(success, formData, $upldTableRowFilename, $upldTableRowGroup, $(row));
+						
+						$upldTableRoot.trigger("update");
+					}
+				});
+			} else {
+				$upldTableRowIndicator.removeClass("glyphicon-chevron-up").addClass("glyphicon-repeat");
+				proprocessor(false, formData, $upldTableRowFilename, $upldTableRowGroup, $(row))
+			}
+		});
+		return false; // Stop propagation
+	});
+	
+	$upldTableRoot.hide();
 
 	$(this).on("dragover", false); // Stop propagation
 
@@ -98,27 +93,28 @@ jQuery.fn.uploader = function(processor) {
 		if(!event.originalEvent.dataTransfer.files.length) return;
 
 		var $upldFiles = $(event.originalEvent.dataTransfer.files);
-		var $upldTable = $(this).find("table");
-        var $upldBody = $upldTable.find("tbody");
-        var $upldRowTemplate = $upldBody.find("template");
-        var $upldSubmit = $upldBody.find("button.upld-submit");
+		//var $upldTable = $(this).find("table");
+        //var $upldBody = $upldTable.find("tbody");
+        //var $upldRowTemplate = $upldBody.find("template");
+        //var $upldSubmit = $upldBody.find("button.upld-submit");
+        var $upldTableRowTemplate = $upldTableBody.find("template");
 
         $upldFiles.each(function(i, file) {
-			var $upldRow = $($upldRowTemplate.html());
-			$upldRow.data("file", file);
+			var $upldTableRow = $($upldTableRowTemplate.html()).hide();
 			
-			$upldRow.find("input.upld-filename").attr("placeholder", file.name);
+			$upldTableRow.data("file", file);
+			$upldTableRow.find("input[data-upld-filename]").attr("placeholder", file.name);
 			
-			$upldRow.find("button.upld-dismiss").click(function(event) {
-				$upldRow.remove();
-				$upldTable.trigger("update");
+			$upldTableRow.find("button[data-upld-dismiss]").click(function(event) {
+				$upldTableRow.remove();
+				$upldTableRoot.trigger("update");
 			});
 			
-            $upldBody.prepend($upldRow);
-            $upldRow.fadeIn();
+            $upldTableBody.prepend($upldTableRow);
+            $upldTableRow.fadeIn();
         });
         
-        $upldTable.trigger("update");
+        $upldTableRoot.trigger("update");
         
         return false; // Stop propagation
 	});
@@ -127,7 +123,9 @@ jQuery.fn.uploader = function(processor) {
 }
 
 $(function() {
-	$(".uploader").uploader(function(formData, $filename, $row) {
-		return $filename.val(); // Make sure we've got a filename (just a demo)!
+	$(".uploader").uploader(function(formData, $upldTableRowFilename, $upldTableRowGroup, $row) {
+		return $upldTableRowFilename.val(); // Make sure we've got a filename (just a demo)!
+	}, function(success, formData, $upldTableRowFilename, $upldTableRowGroup, $row) {
+		$upldTableRowGroup.removeClass("has-success has-warning").addClass(success? "has-success" : "has-warning");
 	});
 });
